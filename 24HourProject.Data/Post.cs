c81fe160e882f5e5be93a6041fcb241902265ccd
Mark Suverkrup <marksuverkrup@gmail.com>
2021-07-19 15:49:01 -0400

    Data Class Added

---
+++ 24HourProject.Data/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourProject.Data
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public List Comments { get; set; }
+        public List Likes { get; set; }
+        public Guid AuthorId { get; set; }
+    }
+}
