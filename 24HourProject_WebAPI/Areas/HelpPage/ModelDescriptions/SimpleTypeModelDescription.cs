dda894794a79e78688f7c37db00ffceae038454b
Mark Suverkrup <marksuverkrup@gmail.com>
2021-07-19 13:47:31 -0400

    Add project files.

---
+++ 24HourProject_WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace _24HourProject_WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ 24HourProject_WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_24HourProject_WebAPI.WebApiApplication" Language="C#" %>
