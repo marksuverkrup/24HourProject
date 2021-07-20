f21ea9fe9c2c224b52dea4afeada3b0d11718330
Jim Stewart <jamstew62@gmail.com>
2021-07-20 11:47:46 -0400

    jim 2nd push

---
+++ 24HourProject.Models/ReplyCreate.cs
@@ -0,0 +1,32 @@
+﻿using _24HourProject.Data;
+using Microsoft.Build.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourProject.Models
+{
+    class ReplyCreate
+    {
+        [System.ComponentModel.DataAnnotations.Required]
+                
+        [ForeignKey(nameof(Comment))]
+        public string CommentId { get; set; }
+        public virtual Comment Comment { get; set; }
+
+        public int ReplyId { get; set; }
+
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(100, ErrorMessage = "There are too many characters in the field.")]
+        public string ReplyText { get; set; }
+
+        public Guid ReplyAuthorId { get; set; }
+
+
+
+    }
+}
---
+++ 24HourProject.Services/ReplyService.cs
@@ -0,0 +1,124 @@
+﻿using _24HourProject.Data;
+using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ReplyService
+    {
+        private readonly Guid _userId;
+        public ReplyService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+    }
+
+    public bool CreateReply(ReplyCreate model)
+    {
+        var entity =
+            new Reply()
+            {
+                CommentId = model.CommentId,
+                OwnerId = _userId,
+                ReplyText = model.ReplyText,
+            };
+
+        using (var ctx = new ApplicationDbContext())
+        {
+            ctx.Reply.Add(entity);
+            return ctx.SaveChanges() == 1;
+        }
+    }
+
+    public IEnumerable<ReplyListItem> GetReplys()
+    {
+        using (var ctx = new ApplicationDbContext())
+        {
+            var query =
+                ctx
+                .Replys
+                .Where(e => e.OwnerId == _userId)
+                .Select(
+                    e =>
+                    new ReplyListItem
+                    {
+                        ReplyId = e.ReplyId,
+                        ReplyText = e.ReplyText,
+                    }
+                    );
+            return query.ToArray();
+        }
+    }
+    public ReplyDetail GetReplyById(int id)
+    {
+        using (var ctx = new ApplicationDbContext())
+        {
+            var entity =
+                ctx
+                .Replys
+                .Single(e => e.ReplyId == id && e.OwnerId == _userId);
+            return
+                new ReplyDetail
+                {
+                    ReplyId = entity.ReplyId,
+                    ReplyText = entity.ReplyText,
+                };
+
+        }
+    }
+
+    public ReplyDetail GetReplyByAuthor(int replyauthorid)
+    {
+        using (var ctx = new ApplicationDbContext())
+        {
+            var entity =
+                ctx
+                .Replys
+                .Single(e => e.ReplyAuthorId == id && e.OwnerId == _userId);
+            return
+                new ReplyDetail
+                {
+                    ReplyId = entity.ReplyId,
+                    ReplyText = entity.ReplyText,
+                    ReplyAuthorId = entity.ReplyAuthorId
+                };
+
+        }
+    }
+
+    public bool UpdateReply(ReplyEdit model)
+    {
+        using (var ctx = new ApplicationDbContext())
+        {
+            var entity =
+                ctx
+                .Replys
+                .Single(e => e.ReplyId == model.ReplyId && e.OwnerId == _userId);
+
+            entity.ReplyId = model.ReplyId;
+            entity.ReplyText = model.ReplyText;
+
+            return ctx.SaveChanges() == 1;
+        }
+    }
+
+    public bool DeleteReply(int ReplyId)
+    {
+        using (var ctx = new ApplicationDbContext())
+        {
+            var entity =
+                ctx
+                .Replys
+                .Single(e => e.ReplyId == ReplyId && e.OwnerId == _userId);
+
+            ctx.Replys.Remove(entity);
+
+            return ctx.SaveChanges() == 1;
+        }
+    }
+}
